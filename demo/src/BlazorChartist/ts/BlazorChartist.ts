557f9a988f2d27b8e1baeb99dcd35afd90fa16a5
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-01-20 14:44:21 +0000

    ShowArea option

--- demo/src/BlazorChartist/ts/BlazorChartist.ts
+++ demo/src/BlazorChartist/ts/BlazorChartist.ts
@@ -1,23 +1,13 @@
-﻿import { IChartistBase, IChartOptions, IChartistData, Bar, Line, IChartistSeriesData } from 'chartist';
+﻿import * as Chartist from 'chartist';
 
 export default class BlazorChartist {
-    static init(elem: Element, type: 'Bar' | 'Line') {
-        const data: IChartistData = { series: [] };
-
-        let chart: IChartistBase<IChartOptions>;
-        switch (type) {
-            case 'Bar':
-                chart = new Bar(elem, data);
-                break;
-            case 'Line':
-                chart = new Line(elem, data);
-                break;
-        }
-
-        elem['_chart'] = chart;
+    static init(elem: Element, type: 'Bar' | 'Line' | 'Pie') {
+        const constructor = Chartist[type];
+        elem['_chart'] = new constructor(elem, { series: [] });
     }
 
-    static update(elem: Element, data: IChartistData) {
-        elem['_chart'].update(data);
+    static update(elem: Element, data: Chartist.IChartistData, options: Chartist.IChartOptions) {
+        //data.series = (data.series[0] as any).data;
+        elem['_chart'].update(data, options);
     }
 }
