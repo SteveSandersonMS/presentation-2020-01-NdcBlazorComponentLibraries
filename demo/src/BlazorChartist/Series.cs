dfbd7b79433e2af22ff8802a6e573cb0af93fc07
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-01-20 15:28:54 +0000

    Add some XML docs

---
+++ demo/src/BlazorChartist/Series.cs
@@ -0,0 +1,40 @@
+﻿using BlazorChartist.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace BlazorChartist
+{
+    /// <summary>
+    /// Adds a data series to the enclosing <see cref="Chart"/> component.
+    /// </summary>
+    public class Series : ComponentBase, IDisposable
+    {
+        [CascadingParameter] public Chart OwnerChart { get; set; }
+
+        [Parameter] public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data values associated with the series.
+        /// </summary>
+        [Parameter] public IEnumerable<double> Values { get; set; }
+
+        private readonly SeriesData data = new SeriesData();
+
+        protected override void OnParametersSet()
+        {
+            data.Name = Name;
+            data.Data = Values;
+        }
+
+        protected override void OnInitialized()
+        {
+            OwnerChart.Data.Series.Add(data);
+        }
+
+        void IDisposable.Dispose()
+        {
+            OwnerChart.Data.Series.Remove(data);
+        }
+    }
+}
