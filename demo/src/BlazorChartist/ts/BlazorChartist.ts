107da296423e3bc9721e51ff72d0307abf08b8ad
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-01-20 13:08:04 +0000

    Add series element

--- demo/src/BlazorChartist/ts/BlazorChartist.ts
+++ demo/src/BlazorChartist/ts/BlazorChartist.ts
@@ -1,27 +1,43 @@
 ﻿import * as Chartist from 'chartist';
 
 export default class BlazorChartist {
-
-    static createChart(options: BlazorChartistOptions) {
+    static createChart(options: AddChartOptions) {
         const data: Chartist.IChartistData = {
             labels: options.labels,
-            series: options.series,
+            series: [],
         };
 
+        let chart: Chartist.IChartistBase<Chartist.IChartOptions>;
         switch (options.type) {
             case 'Bar':
-                new Chartist.Bar(options.elem, data);
+                chart = new Chartist.Bar(options.elem, data);
                 break;
             case 'Line':
-                new Chartist.Line(options.elem, data);
+                chart = new Chartist.Line(options.elem, data);
                 break;
         }
+
+        BlazorChartist.setChart(options.elem, chart);
+    }
+
+    static addSeries(elem: Element, id: string, values: number[]) {
+        const chart = BlazorChartist.getChart(elem);
+        const newSeriesData: Chartist.IChartistSeriesData = { meta: id, data: values };
+        chart.data.series.push(newSeriesData as any);
+        chart.update(chart.data);
+    }
+
+    private static getChart(elem: Element): Chartist.IChartistBase<Chartist.IChartOptions> {
+        return elem['_chart'];
+    }
+
+    private static setChart(elem: Element, chart: Chartist.IChartistBase<Chartist.IChartOptions>) {
+        elem['_chart'] = chart;
     }
 }
 
-interface BlazorChartistOptions {
+interface AddChartOptions {
     elem: Element,
     type: 'Bar' | 'Line',
     labels: string[],
-    series: number[][],
 }
