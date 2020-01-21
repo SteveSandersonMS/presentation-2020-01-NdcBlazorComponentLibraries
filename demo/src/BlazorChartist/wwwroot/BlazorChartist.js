9e06f3100c9cf97e02144bca26d29d9190f9c0e6
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-01-21 13:43:36 +0000

    [3] With chartist files and empty Chart.razor

---
+++ demo/src/BlazorChartist/wwwroot/BlazorChartist.js
@@ -0,0 +1,11 @@
+﻿var BlazorChartist = {
+
+    createChart: function (elem) {
+        // An example pasted from chartist.js docs
+        new Chartist.Line(elem, {
+            labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+            series: [[5, 2, 4, 2, 0]]
+        });
+    }
+
+};
